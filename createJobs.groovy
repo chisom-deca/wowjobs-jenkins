3eb33f59de92f60fc45d2aa3ec2ad06be7f58062
chisom-deca <iwowoemmanuelc@gmail.com>
2020-12-29 19:19:00 +0100

    add job for cortex viewjob backend client

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('Cortex-viewjob-Backend-Client-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tardisone/Cortex-Viewjob-Backend-Client'
+                    }
+                    branch 'cortex-viewjob-backend-client-chisom-deca-check-permissions'
+                }
+            }
+        }
+    }
+}
